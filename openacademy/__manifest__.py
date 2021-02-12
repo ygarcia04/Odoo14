a36928d618633ece16c3b7806ed3a19a7eeebcd0
ygarcia04 <funesyessihotmail.com>
2021-02-12 13:52:16 -0600

    Syntaxis

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -26,7 +26,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        '/views/openacademy.xml',
+        'views/openacademy.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
