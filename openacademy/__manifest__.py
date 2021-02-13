6e732b89a740e577278e2a756a457b369061e705
ygarcia04 <funesyessihotmail.com>
2021-02-12 22:47:07 -0600

    [ADD] session

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -27,7 +27,8 @@
     'data': [
         'security/ir.model.access.csv',
         'views/openacademy.xml',
-        'views/course.xml'
+        'views/course.xml',
+        'views/session.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
