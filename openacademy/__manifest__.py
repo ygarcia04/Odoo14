78ed5be00ae5314c395ca30820697dac748fb224
ygarcia04 <funesyessihotmail.com>
2021-02-14 17:08:34 -0600

    [ADD] Reporting

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -21,7 +21,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
 
     # always loaded
     'data': [
@@ -30,7 +30,9 @@
         'views/openacademy.xml',
         'views/course.xml',
         'views/session.xml',
-        'views/partner.xml'
+        'views/partner.xml',
+        'views/session_board.xml',
+        'reports.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
